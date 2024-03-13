d818f4a6aa9bfd561f0d1ce75b303fa0cce3a399
tinytom3 <131354578+tinytom3@users.noreply.github.com>
2024-03-13 22:45:33 +0000

    New hacking console
    different mini games to choose from when hacking/decrypting etc

---
+++ server-tools/lua/entities/hacking_console/init.lua
@@ -0,0 +1,49 @@
+AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
+AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
+include('shared.lua')
+
+util.AddNetworkString("openhackingmenu3")
+util.AddNetworkString("normalchoice")
+util.AddNetworkString("remotechoice")
+util.AddNetworkString("playerdead")
+function ENT:Initialize()
+ 
+	self:SetModel( "models/lordtrilobite/starwars/isd/imp_console_medium02.mdl" )
+	self:PhysicsInit( SOLID_VPHYSICS )      -- Make us work with physics,
+	self:SetMoveType( MOVETYPE_VPHYSICS )   -- after all, gmod is a physics
+	self:SetSolid( SOLID_VPHYSICS )         -- Toolbox
+ 
+        local phys = self:GetPhysicsObject()
+	if (phys:IsValid()) then
+		phys:Wake()
+	end
+    self:SetVar("usedd", false)
+    self:SetVar("isdonee", false)
+    self:SetUseType( SIMPLE_USE )
+    net.Receive("dowloaddata", function()
+        PrintMessage(HUD_PRINTTALK, "[Database] Data Succesfully Dowloaded")
+        self:SetVar("usedd", false)
+        self:SetVar("isdonee", true)
+    end)
+end
+ 
+function ENT:Use( activator, caller )
+    if (activator:GetNWBool("isableto")) then
+        if (self:GetVar("usedd") == true) then
+            activator:PrintMessage(HUD_PRINTTALK, "This Database is already being hacked")
+        else
+            self:SetNWInt("actindex", activator:UserID())
+            if (self:GetVar("isdonee") == false) then
+                net.Start("openhackingmenu3")
+                net.WriteBool( true )
+                net.Send(activator)
+                CheckAlivestuff(activator)
+                self:SetVar("usedd", true)
+            else
+                activator:PrintMessage(HUD_PRINTTALK, "This Database has already been hacked")
+            end
+        end
+    else
+        activator:PrintMessage(HUD_PRINTTALK, "You Dont have hacking tools")
+    end
+end
---
+++ server-tools/lua/entities/hacking_console/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.PrintName= "Hackable Console"
+ENT.Author= "Tomery"
+ENT.Contact= ""
+ENT.Purpose= "A Database that can be hacked"
+ENT.Instructions= "Press E to start hacking"
+ENT.Category= "Hackable Rayshield"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
