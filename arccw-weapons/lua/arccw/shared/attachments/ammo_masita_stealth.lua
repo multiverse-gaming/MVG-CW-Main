15e92d768c8295c9f1815747d7bb0c0f3a83fb1f
MontyMVG <monty.jwa@gmail.com>
2024-07-14 12:42:20 +0100

    Stealth Round & Valken Fix

--- arccw-weapons/lua/arccw/shared/attachments/ammo_masita_stealth.lua
+++ arccw-weapons/lua/arccw/shared/attachments/ammo_masita_stealth.lua
@@ -18,7 +18,8 @@ att.AutoStats = true
 att.Override_PhysTracerProfile = 7
 att.Override_TracerNum = 0
 
---att.Override_Tracer = "lrb_11_tracer"
+att.Override_Tracer = "tracer_stealth"
+att.Override_TracerCol = Color(0, 0, 0, 0)
 att.Override_MuzzleEffect = false
 
 --[[att.Mult_MalfunctionMean = 1.3
