7776d7db773dc285dd43dc0ebec780980586c508
KalinKD339 <kalin911a@gmail.com>
2024-09-26 13:40:45 +0300

    adding GC Barrage

--- zeus/lua/autorun/41st_snow_arms.lua
+++ zeus/lua/autorun/41st_snow_arms.lua
@@ -6,22 +6,10 @@ list.Set( "PlayerOptionsModel", "41st Snow Ranger", "models/zeus/41st_ranger_sno
 player_manager.AddValidModel( "41st Snow Ranger", "models/zeus/41st_ranger_snow.mdl" )
 player_manager.AddValidHands( "41st Snow Ranger", "models/zeus/41st_trooper_snow_arms.mdl",0,"10000000" )
 
+list.Set( "PlayerOptionsModel", "41st Barrage", "models/zeus/41st_barrage.mdl" )
+player_manager.AddValidModel( "41st Barrage", "models/zeus/41st_barrage.mdl" )
+player_manager.AddValidHands( "41st Barrage", "models/aussiwozzi/cgi/base/41st_trooper_arms.mdl",0,"10000000" )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+list.Set( "PlayerOptionsModel", "Phase 2 CG Pointer (CGI)", "mmodels/zeus/cg_pointer.mdl" )
+player_manager.AddValidModel( "Phase 2 CG Pointer (CGI)", "models/zeus/cg_pointer.mdl" )
+player_manager.AddValidHands( "Phase 2 CG Pointer (CGI)", "models/aussiwozzi/cgi/base/CG_trooper_arms.mdl",0,"10000000" )
