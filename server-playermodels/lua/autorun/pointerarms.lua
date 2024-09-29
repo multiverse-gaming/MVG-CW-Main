4ffbf87a7b1c202172d95efcbf9eee327818184f
KalinKD339 <kalin911a@gmail.com>
2024-09-29 15:23:14 +0300

    removal of the below file
    pointer's arms are stored in 41st_snow_arms. this is unneeded

--- server-playermodels/lua/autorun/pointerarms.lua
+++
@@ -1,3 +0,0 @@
-list.Set( "PlayerOptionsModel", "Phase 2 CG Pointer (CGI)", "models/zeus/cg_pointer.mdl" )
-player_manager.AddValidModel( "Phase 2 CG Pointer (CGI)", "models/zeus/cg_pointer.mdl" )
-player_manager.AddValidHands( "Phase 2 CG Pointer (CGI)", "models/aussiwozzi/cgi/base/CG_trooper_arms.mdl",0,"10000000" )
