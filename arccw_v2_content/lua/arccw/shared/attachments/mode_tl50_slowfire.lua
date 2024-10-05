0ad25c73914b6df0d548e2be017db5c6677ac280
mitsuru <codeberg@yamamech.sh>
2024-10-05 20:16:28 +0100

    Added missing slowfire TL-50 mode

---
+++ arccw_v2_content/lua/arccw/shared/attachments/mode_tl50_slowfire.lua
@@ -0,0 +1,27 @@
+att.PrintName = "Overcharged Blaster Frame"
+att.Free = true
+att.HideIfUnavailable = true
+
+att.SortOrder = 20
+att.Icon = Material("interfaz/armas/sw_powercore.png", "smooth mips")
+att.Description = "Higher pressure compression allows for more powerful shots, at the cost of fire-rate and ammo capacity"
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.Desc_Neutrals = {
+}
+att.Slot = "tl50_frame"
+
+att.AutoStats = true
+
+att.Silencer = false
+
+att.Mult_Damage = 25 / 35 -- 25 Damage
+att.Mult_DamageMin = 15 / 35
+att.Mult_RPM = 450 / 850
+att.Add_ClipSize = -20
+
+att.Mult_AccuracyMOA = 5.7 / 15
+
+att.Mult_RecoilSide = 0.11 / 0.35
