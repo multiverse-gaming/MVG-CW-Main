7186b12a1a2720fde6a7ac305c80be22728135c2
Painitess <RoryBurns2001@hotmail.co.uk>
2024-10-06 16:44:33 +0100

    501st Perks

---
+++ arccw-weapons/lua/arccw/shared/attachments/perk_trp_run_gun.lua
@@ -0,0 +1,58 @@
+att.PrintName = "Run & Gun"
+
+att.Icon = Material("entities/perks/stormtrooper.png")
+
+att.Description = [[Ability to run & gun, though at a cost.]]
+
+att.Desc_Pros = {}
+
+att.Desc_Cons = {}
+
+att.Desc_Neutrals = {}
+
+
+
+att.Slot = {"dc15le_perk"}
+
+
+
+att.AutoStats = true
+
+att.NotForNPC = true
+
+att.Free = false
+att.HideIfUnavailable = false
+
+att.Mult_MoveSpeed = 1
+
+att.Mult_JumpDispersion = 1
+
+att.Mult_AccuracyMOA = 1
+
+att.Mult_HipDispersion = 1.5
+
+att.Mult_MoveDispersion = 0
+
+
+
+att.Mult_DrawTime = 1
+
+att.Mult_HolsterTime = 1
+
+att.Mult_SightTime = 1
+
+
+
+att.Mult_SpeedMult = 1
+
+att.Mult_SightedSpeedMult = 1
+
+att.Mult_ReloadTime = 1
+
+att.Mult_Recoil = 1.5
+
+att.Mult_RecoilSide = 1.35
+
+att.Mult_VisualRecoilMult = 1.3
+
+att.Override_ShootWhileSprint = true
---
+++ arccw-weapons/lua/arccw/shared/attachments/perk_weapon_handling.lua
@@ -0,0 +1,28 @@
+att.PrintName = "Improved Handling"
+
+att.Icon = Material("entities/perks/stormtrooper.png")
+
+att.Description = [[Practice with the weapon allows for improved accuracy and better stability]]
+
+att.Desc_Pros = {}
+
+att.Desc_Cons = {}
+
+att.Desc_Neutrals = {}
+
+
+
+att.Slot = {"dc15le_perk"}
+
+att.Free = true
+att.HideIfUnavailable = true
+
+att.AutoStats = true
+
+att.NotForNPC = true
+
+att.Mult_Recoil = 0.80
+
+att.Mult_RecoilSide = 0.80
+
+att.Mult_VisualRecoilMult = 0.8
